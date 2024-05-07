d00dee9bbe145a1a4dd0096b4d2b984b291137ab
restructrishi <121714645+restructrishi@users.noreply.github.com>
2024-05-07 20:24:28 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role", {
+            strings: [
+                "Web Developer",
+                "Java Developer",
+                "Coder",
+            ],
+            loop: true,
+            typeSpeed: 100,
+            backSpeed: 80,
+            backDelay: 1000,
+        });
+  
